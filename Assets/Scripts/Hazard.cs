ab5e74473007a3c6e58a26e6e44491ab5c4ad041
Conor Holden <cholden48@gmail.com>
2025-03-09 21:44:36 +0100

    Refine distance function

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -5,7 +5,7 @@ public class Hazard : MonoBehaviour
 {
 
 
-    private const float max_dist = 20.0f;
+    private const float max_dist = 5.0f;
 
     private Gamepad pad;
 
@@ -23,7 +23,14 @@ public class Hazard : MonoBehaviour
         Vector3 object_pos = transform.position;
 
         float distance = Vector3.Distance(object_pos, position);
-        float instensity =  (max_dist - distance) / (max_dist * max_dist);
+
+
+        //Debug.Log($"Distance: {distance}");
+
+        // We want this to be exponetialyl related to the distnace. 
+        // So that it decreases dramaticalty the further it gets away.
+        // Currently does not work like that
+        float instensity =  Mathf.Sqrt(max_dist - distance) / Mathf.Sqrt(max_dist);
         return Mathf.Clamp(instensity, 0.0f, 1.0f);
     }
 
@@ -32,14 +39,16 @@ public class Hazard : MonoBehaviour
     void Update()
     {
 
-        Vector3 old_pos = transform.position;
-        float x_pos = Mathf.PingPong(Time.time, 8.0f) - 4f;
-        transform.position = new Vector3(x_pos, old_pos.y, old_pos.z);
+        //Vector3 old_pos = transform.position;
+        //float x_pos = Mathf.PingPong(Time.time, 8.0f) - 4f;
+        //transform.position = new Vector3(x_pos, old_pos.y, old_pos.z);
 
         float left_intensity = GetIntensity(PlayerManager.instance.left_sensor.transform.position);
         float right_intensity = GetIntensity(PlayerManager.instance.right_sensor.transform.position);
 
         pad = Gamepad.current;
+
+        //Debug.Log($"Vib: L {left_intensity} R {right_intensity}");
 
         if (pad != null)
         {
